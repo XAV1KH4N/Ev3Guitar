3268d026afa3b78fb20cae9eb6612c7265419dda
XAV1KH4N <xavierkhan31@gmail.com>
2022-12-12 16:15:45 +0000

    Fixed bug with guitar

--- Guitar.py
+++ Guitar.py
@@ -4,19 +4,15 @@ from Body import Body
 
 class Guitar:
     def __init__(self, ev3):
-        self.speaker = Speaker(ev3)
+        self.ev3 = ev3
         #self.neck = Neck()
         #self.body = Body()
         
     def start(self):
-        while button.CENTER not in buttons.pressed():
-            self.speaker.play('C4/4') #'C4/4', 'G4/4'
-
-class Speaker:
-    def __init__(self, ev3):
-        self.ev3 = ev3
-        self.TEMPO = 120
-        
+        while Button.CENTER not in self.ev3.buttons.pressed():
+            self.play('C4/4') #'C4/4', 'G4/4'
+    
     def play(self, chord):
-        self.ev3.speaker.play_notes([chord], tempo=self.TEMPO)
+        self.ev3.speaker.play_notes([chord])
+    
         
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-#from Guitar import Guitar
-
-from Neck import Neck
-from Body import Body
-from Guitar import Guitar, Speaker
+from Guitar import Guitar
 
 # Create your objects here.
 ev3 = EV3Brick()
 
 # Write your program here.
 guitar = Guitar(ev3)
-
+guitar.start()
 print("done")
